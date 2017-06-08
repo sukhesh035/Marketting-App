3d9b4ec27bd8876e0ef2b1f61f3a671c98eee008
Abhinavkumar61 <abhinav.k0217@gmail.com>
2017-06-08 01:24:42 -0700

    made changes in css

--- public/list/list.js
+++ public/list/list.js
@@ -3,10 +3,8 @@ app.controller("list_controller", ["$scope", "$http", function ($scope, $http) {
     $http.get('data/mockData.json').then(function (data) {
         $scope.mockList = data.data.data;
         console.log($scope.mockList);
-//$scope.name = "sai";
-//        $scope.arr = $scope.mockList.data;
-//        console.log($scope.arr);
+
     });
-    
-    
+
+
 }]);

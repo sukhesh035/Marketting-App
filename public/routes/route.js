a58cbd73f2b19a3be555256ebfa91cdbb5d099b6
sjeksani <sharathjeksani@gmail.com>
2017-06-09 16:15:26 -0700

    added route to contact form and updated css

--- public/routes/route.js
+++ public/routes/route.js
@@ -1,5 +1,5 @@
 var app = angular.module("routes_module", ['ui.router']);
-app.config(function ($urlRouterProvider, $stateProvider) {
+app.config(function($urlRouterProvider, $stateProvider) {
     $urlRouterProvider.otherwise('/home');
 
     $stateProvider.state('home', {
@@ -14,6 +14,11 @@ app.config(function ($urlRouterProvider, $stateProvider) {
     $stateProvider.state('list', {
         url: '/list',
         templateUrl: 'list/list.html'
+    });
+
+    $stateProvider.state('contact', {
+        url: '/contact',
+        templateUrl: 'contact/contact.html'
     });
 
 });

1ae7d6101b607976d44f91b0ed137e03feb8f6ca
sukhesh035 <sukhesh.nri@gmail.com>
2017-06-09 00:41:47 -0700

    Added DB hosting

--- gulpFile.js
+++ gulpFile.js
@@ -6,12 +6,31 @@ var mongojs = require('mongojs');
 var db = mongojs('Marketing', ['Marketing_Student']);
 var bodyParser = require('body-parser');
 
+var MongoClient = require('mongodb').MongoClient
+    , format = require('util').format;
+
+var collection;
+
+
+MongoClient.connect('mongodb://sukhesh.nri:ABCabc123$@ds015334.mlab.com:15334/marketing',function(err, db) {
+    if(err) throw err;
+
+    collection = db.collection('marketing_student');
+    collection.insert({a:2}, function(err, docs) {
+        collection.count(function(err, count) {
+            console.log(format("count = %s", count));
+            //db.close();
+        });
+    });
+});
+
+
 
 
 app.use(bodyParser.json());
 app.use('/', express.static('public'));
 app.get('/getStudentList', function (req, res) {
-    db.Marketing_Student.find(function (err, docs) {
+    collection.find(function (err, docs) {
 //        console.log(docs);
         res.send(docs);
     });
@@ -20,7 +39,7 @@ app.post('/addstudent',function(req, res){
 //   console.log(req.body);
     var id = Math.floor(Math.random()*10000);
     req.body.id = id;
-    db.Marketing_Student.insert(req.body,function(err, docs){
+    collection.insert(req.body,function(err, docs){
         res.send("Added Succesfully");
     });
 });
@@ -29,7 +48,7 @@ app.put('/deleteStudent/:id',function(req, res){
     console.log(req.params.id);
     var currentStudentId = Number(req.params.id);
 console.log(typeof (currentStudentId));
-    db.Marketing_Student.remove({"id":currentStudentId},function(err,data){
+    collection.remove({"id":currentStudentId},function(err,data){
         console.log(err);
         console.log(data);
         if(data.ok){

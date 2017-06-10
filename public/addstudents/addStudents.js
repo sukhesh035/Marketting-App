29f7a2af7f8aeba7ba3f4da4535d715978dda7f0
Abhinavkumar61 <abhinav.k0217@gmail.com>
2017-06-10 16:57:20 -0700

    show details working

--- public/addstudents/addStudents.js
+++ public/addstudents/addStudents.js
@@ -1,7 +1,11 @@
 var app = angular.module("addStudents_module", []);
 app.controller("addStudents_controller", ["$scope", "$http", "$state", function ($scope, $http, $state) {
     $scope.studentInfo = {};
+     $scope.studentInfo.client_name=[ ];
+   
+
     $scope.getInfo = function () {
+//$scope.studentInfo.client_name.push($scope.studentInfo.fields);
         $scope.studentInfo.client_name.push($scope.studentInfo.clientName);
         console.log($scope.studentInfo);
         console.log($scope.studentInfo.client_name);
@@ -14,17 +18,17 @@ app.controller("addStudents_controller", ["$scope", "$http", "$state", function 
 
     };
 
-    console.log("$state.params" + $state.params);
-
-    if ($state.current.name == "editStudent") {
-        $http.get("/getStudentDetails/" + $state.params.id).then(function (response) {
+    console.log("$state.params"+$state.params);
+    
+    if($state.current.name == "editStudent"){
+        $http.get("/getStudentDetails/"+$state.params.id).then(function(response){
             console.log(response);
             $scope.studentInfo = response.data[0];
         });
     }
 
-    if ($state.current.name == "studentDetails") {
-        $http.get("/getStudentDetails/" + $state.params.id).then(function (response) {
+    if($state.current.name == "studentDetails"){
+        $http.get("/getStudentDetails/"+$state.params.id).then(function(response){
             console.log(response);
             $scope.studentInfo = response.data[0];
             //            console.log($scope.studentInfo.client_count.length);
